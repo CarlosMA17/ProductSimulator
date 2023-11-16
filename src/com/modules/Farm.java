69912cb803a32e051891cfd7575bea1cb5633c7e
Carlos Martel <102035692+CarlosMA17@users.noreply.github.com>
2023-11-16 09:46:25 +0000

    se ha añadido la clase Farm

--- src/com/modules/Farm.java
+++ src/com/modules/Farm.java
@@ -1,4 +1,47 @@
 package com.modules;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Farm {
+    private List<Vegetable> warehouse = new ArrayList<>();
+    private int maxCapacity;
+
+
+    public Farm(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+
+    public synchronized void plantVegetable(String farmerName, String vegetableName) {
+        while (warehouse.size() >= maxCapacity) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Vegetable vegetable = new Vegetable(vegetableName, farmerName);
+
+        warehouse.add(vegetable);
+
+        System.out.println("el huerto tiene " + warehouse.size() + " cosas plantadas");
+        notify(); //
+    }
+
+    public synchronized Vegetable harvestVegetable(String consumerName) {
+        while (warehouse.isEmpty()) {
+            try {
+                wait();
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Vegetable vegetable = warehouse.remove(0);
+
+        notify();
+
+        return vegetable;
+    }
 }
