19e82de6668c338d6fc9c210ea8697ecc32a1d37
Carlos Martel <102035692+CarlosMA17@users.noreply.github.com>
2023-11-16 10:31:55 +0000

    se añadieron solo un consumidor y un productor y se plantan 5 plantas

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,14 @@ public class Main {
 
         Farm farm = new Farm(5);
 
-        Producer paco = new Producer("Paco", 10, farm);
-        Producer ramon = new Producer("Ramon", 10, farm);
+        Producer paco = new Producer("Paco", 5, farm);
 
-        Consumer faustino = new Consumer("Faustino", 5, farm);
-        Consumer gentleman = new Consumer("Mr. Gentleman", 10, farm);
-        Consumer loquendo = new Consumer("Loquendo", 5, farm);
+        Consumer gentleman = new Consumer("Mr. Gentleman", 5, farm);
 
         Thread pacoThread = new Thread(paco);
-        Thread ramonThread = new Thread(ramon);
-        Thread faustinoThread = new Thread(faustino);
         Thread gentlemanThread = new Thread(gentleman);
-        Thread loquendoThread = new Thread(loquendo);
 
         pacoThread.start();
-        ramonThread.start();
-        faustinoThread.start();
         gentlemanThread.start();
-        loquendoThread.start();
     }
 }
